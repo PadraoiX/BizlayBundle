93d273a8943beea7959a1480d64d92a84d6b2f55
Pablo Sanchez <phackwer@gmail.com>
2015-07-13 11:33:37 -0300

    checkUnique movido para a camada Repository

--- Repository/AbstractRepository.php
+++ Repository/AbstractRepository.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Annotations\IndexedReader;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query;
 use SanSIS\BizlayBundle\Entity\AbstractEntity;
+use \SanSIS\BizlayBundle\Service\ServiceDto;
 
 /**
  * Class AbstractEntityRepository
@@ -13,6 +14,50 @@ use SanSIS\BizlayBundle\Entity\AbstractEntity;
  */
 abstract class AbstractRepository extends EntityRepository
 {
+    /**
+     * Array com erros no processamento da Service
+     *
+     * @var array
+     */
+    protected static $__errors = array();
+
+    /**
+     * Verifica se foram registrados erros no validate ou verify da Service
+     *
+     * @return bool
+     */
+    public function hasErrors()
+    {
+        return (bool) count(self::$__errors);
+    }
+
+    /**
+     * Retorna os erros
+     */
+    public function getErrors()
+    {
+        return self::$__errors;
+    }
+
+    /**
+     * Adiciona uma mensagem ao bus de erros da service
+     *
+     * @param $type - pode ser validação, verificação ou sistema. Outros tipos podem ser criados conforme necessário
+     * @param $message - Mensagem do erro específico.
+     * @param null $level - Em que nível foi encotrado o erro (Dooctrine, Entidade, Service, ou outra Service, por exemplo)
+     * @param null $source - Objeto que causou o erro
+     * @param null $attr - atributo que causou o erro
+     */
+    public function addError($type, $message, $level = null, $source = null, $attr = null)
+    {
+        $i = count(self::$__errors);
+        self::$__errors[$i] = array();
+        self::$__errors[$i]['type'] = $type;
+        self::$__errors[$i]['message'] = $message;
+        self::$__errors[$i]['level'] = $level;
+        self::$__errors[$i]['source'] = $source;
+        self::$__errors[$i]['attr'] = $attr;
+    }
 
     /**
      * Retorn a query genérica de pesquisa para primeiro nível de entidade (Grid de Crud)
@@ -96,5 +141,62 @@ abstract class AbstractRepository extends EntityRepository
     {
         $query = $this->getSearchQuery($searchData);
         return $query->getScalarResult();
+    }
+
+    /**
+     * Verifica atributos unique em registros ativos
+     *
+     * @param  ServiceDto $dto [description]
+     * @return [type]          [description]
+     */
+    public function checkUnique(ServiceDto $dto, AbstractEntity $entity)
+    {
+        $reflx = new \ReflectionClass($this->getEntityName());
+        $reader = new IndexedReader(new AnnotationReader());
+        $props = $reflx->getProperties();
+
+        foreach ($props as $prop) {
+            $annons = $reader->getPropertyAnnotations($prop);
+            if ($prop->getName() != 'id' && isset($annons['Doctrine\ORM\Mapping\Column']->unique) && $annons['Doctrine\ORM\Mapping\Column']->unique) {
+                $getMethod = 'get' . ucfirst($prop->getName());
+                $uniqueParam = $entity->$getMethod();
+                //verificar e adicionar o erro
+                $qb = $this->createQueryBuilder('u');
+                $qb->select('u.id')
+                   ->andWhere(
+                       $qb->expr()->eq('u.' . $prop->getName(), ':param')
+                   )
+                   ->setParameter('param', $uniqueParam);
+
+                $id = $entity->getId();
+                if (is_null($id)) {
+                    $qb->andWhere(
+                        $qb->expr()->isNotNull('u.id')
+                    );
+                } else {
+                    $qb->andWhere(
+                           $qb->expr()->neq('u.id', ':id')
+                       )
+                       ->setParameter('id', $id);
+                }
+
+                $query = $qb->getQuery();
+
+                $boolAttr = $reflx->hasProperty('isActive') ? 'isActive' : ($reflx->hasProperty('flActive') ? 'flActive' : false);
+
+                if ($boolAttr) {
+                    $query->setDQL($query->getDQL() . ' and u.' . $boolAttr . ' = :boolFlag');
+                    $query->setParameter('boolFlag', true);
+                }
+
+                if ($reflx->hasProperty('statusTuple')) {
+                    $query->setDQL($query->getDQL() . ' and u.statusTuple <> 0 ');
+                }
+
+                if (count($query->getScalarResult())) {
+                    $this->addError('verificação', 'Já existe o valor informado para ' . $prop->getName() . '.', 'Repository', get_class($this), $prop->getName());
+                }
+            }
+        }
     }
 }
