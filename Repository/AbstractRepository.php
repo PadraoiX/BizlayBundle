99943f280cef2eef646769e217187fb63f41ee4f
Pablo Sanchez <phackwer@gmail.com>
2015-07-14 15:22:17 -0300

    [FIX] Ajuste para pesquisar datetime

--- Repository/AbstractRepository.php
+++ Repository/AbstractRepository.php
@@ -120,6 +120,31 @@ abstract class AbstractRepository extends EntityRepository
                             $query->setParameter($field, trim($criteria));
                             $and = ' and ';
                         }
+                        if ($pt == 'date' || $pt == 'datetime' || $pt == 'time') {
+
+                            $dql .= $and . 'g.' . $field . ' = :' . $field . ' ';
+                            //@TODO - Melhorar isto depois, pelamordedeus
+                            if (strstr($criteria, 'T')) {
+                                $criteria = explode('T', $criteria);
+                                $time = explode('-', $criteria[1]);
+                                $criteria = $criteria[0] . ' ' . $time[0];
+                            }
+                            if (strstr($criteria, '/')) {
+                                if (strstr($criteria, ':')) {
+                                    $criteria = $class::createFromFormat('d/m/Y H:i:s', $criteria);
+                                } else {
+                                    $criteria = $class::createFromFormat('d/m/Y', $criteria);
+                                }
+                            } else {
+                                if (strstr($criteria, ':')) {
+                                    $criteria = $class::createFromFormat('Y-m-d H:i:s', $criteria);
+                                } else {
+                                    $criteria = $class::createFromFormat('Y-m-d', $criteria);
+                                }
+                            }
+                            $query->setParameter($field, trim($criteria));
+                            $and = ' and ';
+                        }
                     }
                     $count = true;
                 } else {
