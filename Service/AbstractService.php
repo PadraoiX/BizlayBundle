25a07ea7a40f68ada9241755871f2857625ff3be
Pablo Sanchez <phackwer@gmail.com>
2015-08-14 19:57:58 -0300

    Método toCamelCase

--- Service/AbstractService.php
+++ Service/AbstractService.php
@@ -203,4 +203,14 @@ abstract class AbstractService
         }, $input));
         return $r;
     }
+
+    public function toCamelCase( $string, $first_char_caps = false)
+    {
+        if( $first_char_caps == true )
+        {
+            $string[0] = strtoupper($string[0]);
+        }
+        $func = create_function('$c', 'return strtoupper($c[1]);');
+        return preg_replace_callback('/_([a-z])/', $func, $string);
+    }
 }
